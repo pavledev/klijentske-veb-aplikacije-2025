5f0e0b1e718b8a499b4fd93dc4002b2993b47d77
Pavle <pavle_nis@yahoo.com>
2025-02-26 16:53:02 +0100

    Add home page cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,36 @@
 import { Component } from '@angular/core';
 import { FlightService } from '../../services/flight.service';
-import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { AxiosError } from 'axios';
 import { FlightModel } from '../../models/flight.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
     selector: 'app-home',
-    imports: [JsonPipe, NgIf, NgFor],
+    imports: [NgIf, NgFor, MatButtonModule, MatCardModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
 export class HomeComponent
 {
-    public flights: FlightModel[] | null = null;
-    public error: string | null = null;
+    public flights: FlightModel[] | null = null
+    public error: string | null = null
 
     constructor()
     {
-        FlightService.getFlights()
+        FlightService.getFlights(0, 3)
             .then(rsp => this.flights = rsp.data.content)
-            .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`);
+            .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`)
+    }
+
+    public formatDate(iso: string)
+    {
+        return new Date(iso).toLocaleString('sr-RS')
+    }
+
+    public generateDestinationImage(dest: string)
+    {
+        return `https://img.pequla.com/destination/${dest.split(' ')[0].toLowerCase()}.jpg`
     }
 }
